30ea4991601fb8dfc39128a341b9f0f16f5c3fb1
Nadina Oates <nadinaoates@gmail.com>
2023-08-19 13:18:42 -0500

    adding contract interaction

--- web3-dapp/backend/src/app.controller.ts
+++ web3-dapp/backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import {TransactionReceipt} from 'ethers';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('get-address')
+  getTokenAddress(): string {
+    return this.appService.getTokenAddress();
+  }
+
+  @Get('get-total-supply')
+  getTotalSupply(): Promise<bigint> {
+    return this.appService.getTotalSupply();
+  }
+  
+  @Get('get-token-balance')
+  getTokenBalance(@Query('address') address: string): Promise<bigint> {
+    return this.appService.getTokenBalance(address);
+  }
+  
+  @Get('get-transaction-status')
+  getTransactionStatus(@Query('txHash') txHash: string): Promise<number> {
+    return this.appService.getTransactionStatus(txHash);
+  }
+  
+  @Get('get-transaction-receipt')
+  getTransactionReceipt(@Query('txHash') txHash: string): Promise<TransactionReceipt> {
+    return this.appService.getTransactionReceipt(txHash);
   }
 }
--- web3-dapp/backend/src/app.service.ts
+++ web3-dapp/backend/src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import {TransactionReceipt, ethers} from 'ethers';
+import * as tokenJson from "./assets/MyToken.json"
+
+const TOKEN_ADDRESS = "0xba45143cC39BA70025d1d125c873Ee548aC0a166";
 
 @Injectable()
 export class AppService {
+
+  provider: ethers.Provider;
+  wallet: ethers.Wallet;
+  contract: ethers.Contract;
+
+  constructor(){
+    this.provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+    this.wallet = new ethers.Wallet(process.env.PRIVATE_KEYS?.split(',')[0] ?? "", this.provider);
+    this.contract = new ethers.Contract(TOKEN_ADDRESS, tokenJson.abi, this.wallet);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  getTokenAddress(): string {
+    return TOKEN_ADDRESS;
+  }
+
+  getTotalSupply(): Promise<bigint> {
+    return this.contract.totalSupply();
+  }
+
+  getTokenBalance(address: string): Promise<bigint> {
+    return this.contract.balanceOf(address);
+  }
+  
+  async getTransactionStatus(txHash: string): Promise<number> {
+    const txReceipt = await this.provider.getTransactionReceipt(txHash)
+    return txReceipt.status;
+  }
+  
+  async getTransactionReceipt(txHash: string): Promise<TransactionReceipt> {
+    return await this.provider.getTransactionReceipt(txHash);
+  }
+  
 }
+
